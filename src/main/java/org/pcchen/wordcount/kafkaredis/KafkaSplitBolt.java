579570ffbc022c77b6ed1ebe57f483ad5b59b98a
pcchen <cpc_geek@163.com>
2017-03-16 15:51:49 +0800

    对从kafka传递的数据在storm进行处理功能实现

--- src/main/java/org/pcchen/wordcount/kafkaredis/KafkaSplitBolt.java
+++ src/main/java/org/pcchen/wordcount/kafkaredis/KafkaSplitBolt.java
@@ -4,8 +4,15 @@ import backtype.storm.task.OutputCollector;
 import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+import com.esotericsoftware.kryo.io.Output;
+import org.apache.commons.lang.ArrayUtils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -13,18 +20,29 @@ import java.util.Map;
  * Created by ceek on 2017-03-15 23:21.
  */
 public class KafkaSplitBolt extends BaseRichBolt {
+    private OutputCollector outputCollector;
+
     @Override
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
-
+        this.outputCollector = outputCollector;
     }
 
     @Override
     public void execute(Tuple tuple) {
+        List<Object> strList = new ArrayList<Object>();
 
+        //1、数据如何获取
+        byte[] juzi = (byte[])tuple.getValueByField("bytes");
+        //2、进行切割
+        String[] strings = new String(juzi).split(" ");
+        for(String value : strings) {
+//            strList.add(value);
+            outputCollector.emit(new Values(value));
+        }
     }
 
     @Override
     public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
-
+        outputFieldsDeclarer.declare(new Fields("splitLine"));
     }
 }
--- src/main/java/org/pcchen/wordcount/kafkaredis/KafkaTopologyDriver.java
+++ src/main/java/org/pcchen/wordcount/kafkaredis/KafkaTopologyDriver.java
@@ -1,16 +1,29 @@
 package org.pcchen.wordcount.kafkaredis;
 
+import backtype.storm.Config;
+import backtype.storm.LocalCluster;
+import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
 import storm.kafka.*;
 import storm.kafka.KafkaSpout;
+
+        import java.util.prefs.BackingStoreException;
 
 /**
  * storm读取kafka中的数据的主题类topology
  * Created by ceek on 2017-03-15 23:22.
  */
-public class KafkaTopologyDriver {
+public class KafkaTopologyDriver  {
     public static void main(String[] args) {
         TopologyBuilder topologyBuilder = new TopologyBuilder();
-        topologyBuilder.setSpout("kafkaSpout", new KafkaSpout(new SpoutConfig(new ZkHosts(""), "pc", "/storm", "")));
+        topologyBuilder.setSpout("kafkaSpout", new KafkaSpout(new SpoutConfig(new ZkHosts("hadoop01:2181"), "kafka_storm_redis", "/storm", "1")));
+        topologyBuilder.setBolt("splitBolt", new KafkaSplitBolt()).shuffleGrouping("kafkaSpout");
+        topologyBuilder.setBolt("coutBolt", new KafkaCoutBolt()).shuffleGrouping("splitBolt");
+
+        Config conf = new Config();
+
+        StormTopology topology = topologyBuilder.createTopology();
+        LocalCluster cluster = new LocalCluster();
+        cluster.submitTopology("kafkaCountWord", conf, topology);
     }
 }
